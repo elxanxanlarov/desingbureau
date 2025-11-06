fc52c1c2ad9237fddcde3f2f2449e06285ea7825
Elxan <elxanxanlarov@gmail.com>
2025-11-06 05:57:03 +0400

    Fix punctuation in NotFound component message

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ export default function NotFound() {
         <div className="flex flex-col items-center justify-center h-screen w-screen bg-[#adacac] ">
             <h1 className="text-9xl font-bold">404</h1>
             <h1 className="text-4xl font-bold">Səhifə tapılmadı</h1>
-            <p className="text-2xl font-semibold">Hazırlanma aşamasındadır</p>
+            <p className="text-2xl font-semibold">Hazırlanma aşamasındadır.</p>
         </div>
     )
 }
